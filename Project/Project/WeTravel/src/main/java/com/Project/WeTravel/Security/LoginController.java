f5d5adb40ef3cc53221f019eef6e5db4d99a4b73
Alejandro_Rincon_Perez <168574435+AlejandroRinconPerez@users.noreply.github.com>
2025-01-30 17:12:20 -0500

    Starting We Travel

---
+++ Project/Project/WeTravel/src/main/java/com/Project/WeTravel/Security/LoginController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Project.WeTravel.Security;
+
+
+import com.Project.WeTravel.Users.application.UserService;
+import com.Project.WeTravel.Users.domain.CreateUserDTO;
+import com.Project.WeTravel.Users.domain.Users;
+import com.Project.WeTravel.Users.domain.UsersDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private JWTAuthtenticationConfig jwtAuthtenticationConfig;
+
+    @Autowired
+    private UserService userService;
+
+    
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestParam("userName") String username,
+            @RequestParam("password") String password) {
+
+        if (userService.verificarUsername(username, password)) {
+            String token = jwtAuthtenticationConfig.getJWTToken(username);
+
+            LoginUser user = new LoginUser(username, token);
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody CreateUserDTO createUserDTO) {
+        try {
+            if(userService.verificarUsername(createUserDTO.getUserName())== false){
+                return ResponseEntity.badRequest().body("User already exits.");
+            }
+
+            ResponseEntity<UsersDTO> savedUser = userService.createUser(createUserDTO);
+
+            return ResponseEntity.ok("Usuario registrado exitosamente: " + savedUser.getBody().getName());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error al registrar el usuario.");
+        }
+    }
+   
+}
