cf8b59e21f41096a4c4db394a9e9067348020603
Alejandro_Rincon_Perez <168574435+AlejandroRinconPerez@users.noreply.github.com>
2025-01-31 13:16:01 -0500

    Delete Project/WeTravel/src/main/java/com/Project/WeTravel directory

--- Project/WeTravel/src/main/java/com/Project/WeTravel/Utilities/exceptions/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package com.Project.WeTravel.Utilities.exceptions;
-
-public class InvalidInputException extends RuntimeException{
-
-    public InvalidInputException(String message) {
-        super(message);
-    }
-    
-}
--- Project/WeTravel/src/main/java/com/Project/WeTravel/Utilities/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.Project.WeTravel.Utilities.exceptions;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-    
-}
