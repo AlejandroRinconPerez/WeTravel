f437cbae8a5ca2480ea968fecd698c0a9acb8db0
Alejandro_Rincon_Perez <168574435+AlejandroRinconPerez@users.noreply.github.com>
2025-01-31 10:54:56 -0500

    Delete Project/Project/WeTravel directory

--- Project/Project/WeTravel/src/main/java/com/Project/WeTravel/Utilities/exceptions/UniqueViolationException.java
+++
@@ -1,9 +0,0 @@
-package com.Project.WeTravel.Utilities.exceptions;
-
-public class UniqueViolationException extends RuntimeException {
-
-    public UniqueViolationException(String message) {
-        super(message);
-    }
-    
-}
